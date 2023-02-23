543dab5bbd6090e0a62d5de36d3ad0d85e3c382d
David Miles <davidmiles1925@gmail.com>
2023-02-22 19:11:27 -0600

    update image file path

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,62 +4,62 @@ const long = "-94.78542942574806";
 
 const weatherImages = [
   {
-    image: "../../images/weathercard/Day_Clear.png",
+    image: "./images/weathercard/Day_Clear.png",
     isDay: true,
     condition: "Clear",
   },
   {
-    image: "../../images/weathercard/Day_Cloudy.png",
+    image: "./images/weathercard/Day_Cloudy.png",
     isDay: true,
     condition: "Clouds",
   },
   {
-    image: "../../images/weathercard/Day_Rain.png",
+    image: "./images/weathercard/Day_Rain.png",
     isDay: true,
     condition: "Rain",
   },
   {
-    image: "../../images/weathercard/Day_Thunderstorm.png",
+    image: "./images/weathercard/Day_Thunderstorm.png",
     isDay: true,
     condition: "Thunderstorm",
   },
   {
-    image: "../../images/weathercard/Day_Snow.png",
+    image: "./images/weathercard/Day_Snow.png",
     isDay: true,
     condition: "Snow",
   },
   {
-    image: "../../images/weathercard/Day_Fog.png",
+    image: "./images/weathercard/Day_Fog.png",
     isDay: true,
     condition: "Fog",
   },
   {
-    image: "../../images/weathercard/Night_Clear.png",
+    image: "./images/weathercard/Night_Clear.png",
     isDay: false,
     condition: "Clear",
   },
   {
-    image: "../../images/weathercard/Night_Cloudy.png",
+    image: "./images/weathercard/Night_Cloudy.png",
     isDay: false,
     condition: "Clouds",
   },
   {
-    image: "../../images/weathercard/Night_Rain.png",
+    image: "./images/weathercard/Night_Rain.png",
     isDay: false,
     condition: "Rain",
   },
   {
-    image: "../../images/weathercard/Night_Thunderstorm.png",
+    image: "./images/weathercard/Night_Thunderstorm.png",
     isDay: false,
     condition: "Thunderstorm",
   },
   {
-    image: "../../images/weathercard/Night_Snow.png",
+    image: "./images/weathercard/Night_Snow.png",
     isDay: false,
     condition: "Snow",
   },
   {
-    image: "../../images/weathercard/Night_Fog.png",
+    image: "./images/weathercard/Night_Fog.png",
     isDay: false,
     condition: "Fog",
   },
